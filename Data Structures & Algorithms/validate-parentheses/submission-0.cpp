aaeea9a9f3ad0da503b1fcc94a87cc123ef85312
Goswami Anujgiri Mukeshgiri <anujgoswami0212@gmail.com>
2026-09-06 19:56:25 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+
+        for(auto ch : s) {
+            if(ch == '(' || ch == '{' || ch == '[') {
+                stk.push(ch);
+            }
+            else {
+                if(stk.empty()) return false;
+
+                if((ch == ')' && stk.top() == '(')
+                || (ch == '}' && stk.top() == '{')
+                || (ch == ']' && stk.top() == '[')) {
+                    stk.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return stk.empty();
+    }
+};
